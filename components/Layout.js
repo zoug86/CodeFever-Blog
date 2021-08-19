0137345007c932255d75e87bf5bdc377d1721578
Houssem Marzougui <marzouguih86@gmail.com>
2021-08-18 19:40:08 -0700

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Header from './Header'
+
+
+export default function Layout({ title, children, keywords, description }) {
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+                <meta name='keywords' content={keywords} />
+                <meta name='description' content={description} />
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <Header />
+
+            <main className='container mx-auto my-7'>{children}</main>
+        </div>
+    )
+}
+
+
+Layout.defaultProps = {
+    title: 'Welcome to CodeFever',
+    keywords: 'coding, data structure, algorithms, programming',
+    description: 'The best coding practices in programming'
+}
---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+export default function AboutPage() {
+    return (
+        <Layout title='About CodeFever'>
+
+        </Layout>
+    )
+}
