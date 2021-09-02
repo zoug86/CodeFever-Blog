4d6ad14c92954beb5e136bf70e565aa06c0e8856
Houssem Marzougui <marzouguih86@gmail.com>
2021-09-01 20:19:27 -0700

    added new markdown page

--- components/Pagination.js
+++ components/Pagination.js
@@ -20,7 +20,7 @@ export default function Pagination({ currentPage, numPages }) {
                     </Link>
                 )}
                 {Array.from({ length: numPages }, (_, i) => (
-                    <Link href={`/blog/page/${i + 1}`}>
+                    <Link key={i} href={`/blog/page/${i + 1}`}>
                         <li key={i} className='relative block py-2 px-3 leading-tught bg-white
                         border border-gray-300 text-gray-800 mr-1 hover:bg-gray-200 cursor-pointer'>
                             {i + 1}
